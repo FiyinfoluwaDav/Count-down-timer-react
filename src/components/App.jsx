8be2e99fe14bd0ca36e6db3910b25b164132b3e7
Osokoya Fiyinfoluwa David <osokoyafiyinfoluwa@gmail.com>
2025-12-12 23:17:43 +0100

    Add code and files from previous project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,19 @@ function App() {
   return (
     <div>
       <CountdownTimer />
+      <div style={{ backgroundImage: `url(${bottomBg})` }} className="h-[25vh] bg-cover flex justify-center items-center">
+        <div className="flex gap-x-4 text-[#8486A9]">
+          <div>
+            <i className="fa-brands fa-twitter text-3xl hover:text-[#F16A8C] hover:rotate-[360deg] transition-all  duration-500"></i>
+          </div>
+          <div>
+            <i className="fa-brands fa-linkedin text-3xl hover:text-[#F16A8C] hover:rotate-[360deg] transition-all transform duration-500"></i>
+          </div>
+          <div>
+            <i className="fa-brands fa-github text-3xl hover:text-[#F16A8C] hover:rotate-[360deg] transition-all transform duration-500"></i>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
